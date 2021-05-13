465f632f30ac574934f306530469d50996269883
SWARAJ D SHELAVALE <47442263+SwarajShelavale@users.noreply.github.com>
2021-05-13 16:27:30 +0530

    Update 1. Preorder Traversal without recursion.cpp

--- InterviewBit Solutions/Trees/1. Preorder Traversal without recursion.cpp
+++ InterviewBit Solutions/Trees/1. Preorder Traversal without recursion.cpp
@@ -1,5 +1,16 @@
 //Partial Function to calculate Preorder Traversal without recursion
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
 vector<int> Solution::preorderTraversal(TreeNode *root)
 {
 
