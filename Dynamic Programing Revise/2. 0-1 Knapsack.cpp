4e2391bc55b69815b713ae6e8e732dda45917e08
SWARAJ D SHELAVALE <47442263+SwarajShelavale@users.noreply.github.com>
2021-10-05 12:59:40 +0530

    Update 2. 0-1 Knapsack.cpp

--- Dynamic Programing Revise/2. 0-1 Knapsack.cpp
+++ Dynamic Programing Revise/2. 0-1 Knapsack.cpp
@@ -11,18 +11,6 @@ using namespace std;
 
 int dp[1001][10001];
 
-int knapsack(int n, int wt[], int val[], int W)
-{
-    if (n == 0 || W == 0)
-        return 0;
-
-    if (dp[n][W] != -1)
-        return dp[n][W];
-    if (wt[n - 1] <= W)
-        return dp[n][W] = max(val[n - 1] + knapsack(n - 1, wt, val, W - wt[n - 1]), knapsack(n - 1, wt, val, W));
-    else
-        return dp[n][W] = knapsack(n - 1, wt, val, W);
-}
 signed main()
 {
     fast;
