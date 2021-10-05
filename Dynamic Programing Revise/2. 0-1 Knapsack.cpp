1a3fa9bbaadb4330662e1ae41fa1f749af1a4aaa
SWARAJ D SHELAVALE <47442263+SwarajShelavale@users.noreply.github.com>
2021-10-05 12:58:59 +0530

    Create 2. 0-1 Knapsack.cpp

---
+++ Dynamic Programing Revise/2. 0-1 Knapsack.cpp
@@ -0,0 +1,54 @@
+/* Author : Swaraj Shelavale (errorless11) */
+/* 0-1 Knapsack Problem - Top Down Approach*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fast                    \
+    cin.sync_with_stdio(false); \
+    cin.tie(NULL);              \
+    cout.tie(NULL);
+
+int dp[1001][10001];
+
+int knapsack(int n, int wt[], int val[], int W)
+{
+    if (n == 0 || W == 0)
+        return 0;
+
+    if (dp[n][W] != -1)
+        return dp[n][W];
+    if (wt[n - 1] <= W)
+        return dp[n][W] = max(val[n - 1] + knapsack(n - 1, wt, val, W - wt[n - 1]), knapsack(n - 1, wt, val, W));
+    else
+        return dp[n][W] = knapsack(n - 1, wt, val, W);
+}
+signed main()
+{
+    fast;
+    int n = 3;
+    int wt[3] = {10, 20, 30};
+    int val[3] = {60, 100, 120};
+    int W = 50;
+    //memonization
+    memset(dp, -1, sizeof(dp));
+
+    //Top down approch
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int i = 0; i <= W; i++)
+        dp[0][i] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            if (wt[i - 1] <= j)
+                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    cout << dp[n][W];
+    return 0;
+}
